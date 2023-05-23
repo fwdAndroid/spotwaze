666b031c83642a30a9c85acbfb8f1f0fabbe224e
fawad kaleem <fwdkaleem@gmail.com>
2023-05-23 07:34:44 +0500

    Car Park Detail

---
+++ lib/booking/booking_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookingDetails extends StatefulWidget {
+  const BookingDetails({super.key});
+
+  @override
+  State<BookingDetails> createState() => _BookingDetailsState();
+}
+
+class _BookingDetailsState extends State<BookingDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/gps/car_park_detail.dart
+++ lib/gps/car_park_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotwaze/booking/booking_details.dart';
 import 'package:spotwaze/widgetts/drawer.dart';
 
 class CarParkDetail extends StatefulWidget {
@@ -44,6 +45,105 @@ class _CarParkDetailState extends State<CarParkDetail> {
                       hintText: "Eden Garden"),
                 ),
               ),
+              Spacer(),
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "N200/hr",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w900),
+                            ),
+                            Text(
+                              "Lekki Gardens Car Park As",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Esitmated Price/hr",
+                              style: TextStyle(
+                                  color: Color(0xff3B414B),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              width: 40,
+                            ),
+                            Text(
+                              "\$20",
+                              style: TextStyle(
+                                  color: Color(0xff3B414B),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Distance to Venue:",
+                              style: TextStyle(
+                                  color: Color(0xff3B414B),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                            SizedBox(
+                              width: 40,
+                            ),
+                            Text(
+                              "3 mins",
+                              style: TextStyle(
+                                  color: Color(0xff3B414B),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: StadiumBorder(),
+                                backgroundColor: Color(0xff613EEA),
+                                fixedSize: Size(320, 40)),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (builder) => BookingDetails()));
+                            },
+                            child: Text(
+                              "View Car Park",
+                              style: TextStyle(color: Colors.white),
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
             ],
           ),
         ),
